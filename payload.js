e3281dbf021b2528b02f8e2304a4dac75c958d0d
zhirst <zach_hirst@live.com>
2024-03-13 11:47:47 -0500

    placeholder values updated on html && console logs added for javascript

--- payload.js
+++ payload.js
@@ -1,9 +1,6 @@
+console.log('payload.js loaded');
+
 var title = document.title;
-// var inc = document.querySelectorAll('.navbar-title-display-value');
-// var inc_value;
-
-// if(inc.length > 0) { inc_value = inc[0].innerText; }
-// else { inc_value = "Error Grabbing INC"; }
-
-// send the scraped info as a chrome message
 chrome.runtime.sendMessage({title: title});
+
+console.log('payload.js sent message to popup.js');
--- popup.js
+++ popup.js
@@ -1,20 +1,40 @@
 window.addEventListener('load', function (evt) {
+	console.log('popup.js loaded');
+
 	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
 		file: 'payload.js'
 	});;
 });
 
-
+//TODO grab the INC number from title instead of first field 
 chrome.runtime.onMessage.addListener(function (message) {
+	console.log('popup.js received message from payload.js');
+	
 	var title_parts = message.title.split(" | ");
 	var title_first_part = title_parts[0]; 
+
+	console.log('INC: ' + title_first_part);
 	document.getElementById('title').innerHTML = title_first_part;
 });
 
 document.getElementById('sendButton').addEventListener('click', function() {
+	console.log('sendButton clicked');
+
 	var incValue = document.getElementById('title').value;
 	var kbValue = document.getElementById('kb').value;
 	var assignmentGroupValue = document.getElementById('assignmentgroup').value;
+
+	try{
+		if(kbValue.toLowerCase().startsWith('kb') && /^\d{7}$/.test(kbValue.slice(2))) {
+			return 'KB' + kbValue.slice(2);
+		} else {
+			throw new Error('Invalid kbValue format. Expected format is "kb" followed by a 7-digit number.');
+		}
+	} catch(error) {
+		console.log('Error formatting kbValue', error);
+	}
+
+	console.log('Form Values: ', {incValue, kbValue, assignmentGroupValue});
 
 	var formURL = 'https://forms.office.com/Pages/ResponsePage.aspx?id=mthHA3QB002t6zM5yJw19YVBTy6hCVZEnwuXvhFA35JUODdSNlU0RUlVSlRPUk1MT0w2SktCRVRLQyQlQCN0PWcu';
 	var formData = new formData();
@@ -23,17 +43,24 @@ document.getElementById('sendButton').addEventListener('click', function() {
 	formData.append('QuestionId_r015fea291c5f49a69cdb24054e9883d2 QuestionInfo_r015fea291c5f49a69cdb24054e9883d2', kbValue);
 	formData.append('QuestionId_r518dab23ce314eb2818a9c0a9b90c11a QuestionInfo_r518dab23ce314eb2818a9c0a9b90c11a', assignmentGroupValue);
 
+	console.log('Sending form data: ', formData);
+
 	fetch(formUrl, {
         method: 'POST',
         mode: 'no-cors',
         body: formData
     });
 
-
+	console.log('Form data sent');
+	
 	/**
-	 * kb value has a captial kb followed by a 7 digit number (e.g. kb1234567)
 	 * 
-	 * TODO talk to parents about how they want assignment groups data to be filtered 
+	 * 
+	 * TODO name field addition
+	 * 
+	 * TODO test KB value for correct format
+	 * 
+	 * autofilling form from list/live search of assignment groups 
 	 */
 
 	/**
