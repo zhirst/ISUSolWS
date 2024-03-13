24b12963f5518185651c5c0bd387b8d3dc246657
zhirst <zach_hirst@live.com>
2024-03-13 10:42:45 -0500

    branch pilot && formdata creation

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-// Inject the payload.js script into the current tab after the popout has loaded
 window.addEventListener('load', function (evt) {
 	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
 		file: 'payload.js'
@@ -7,35 +6,34 @@ window.addEventListener('load', function (evt) {
 
 
 chrome.runtime.onMessage.addListener(function (message) {
-	// Handle the title of the ticket to grab INC 
 	var title_parts = message.title.split(" | ");
 	var title_first_part = title_parts[0]; 
 	document.getElementById('title').innerHTML = title_first_part;
 });
 
 document.getElementById('sendButton').addEventListener('click', function() {
-	// Get the values from the text boxes
+	var incValue = document.getElementById('title').value;
 	var kbValue = document.getElementById('kb').value;
 	var assignmentGroupValue = document.getElementById('assignmentgroup').value;
 
-	// Create an object to hold the data
-	var data = {
-		title_first_part: title_first_part,
-		kb: kbValue,
-		assignmentGroup: assignmentGroupValue
-	};
+	var formURL = 'https://forms.office.com/Pages/ResponsePage.aspx?id=mthHA3QB002t6zM5yJw19YVBTy6hCVZEnwuXvhFA35JUODdSNlU0RUlVSlRPUk1MT0w2SktCRVRLQyQlQCN0PWcu';
+	var formData = new formData();
 
-	// Send the data to the background page
-	
-	/**
-	 * can you write me this server side script, 
-	 * the data it should have to take in is, ticketnumber, 
-	 * kb, assignment group
-	 */
+	formData.append('QuestionId_r56340fcad985487aaff9636aad9893e7 QuestionInfo_r56340fcad985487aaff9636aad9893e7', incValue);
+	formData.append('QuestionId_r015fea291c5f49a69cdb24054e9883d2 QuestionInfo_r015fea291c5f49a69cdb24054e9883d2', kbValue);
+	formData.append('QuestionId_r518dab23ce314eb2818a9c0a9b90c11a QuestionInfo_r518dab23ce314eb2818a9c0a9b90c11a', assignmentGroupValue);
+
+	fetch(formUrl, {
+        method: 'POST',
+        mode: 'no-cors',
+        body: formData
+    });
+
 
 	/**
-	 * what would the client side code look like for 
-	 * being able to send data using a chrome extension
+	 * kb value has a captial kb followed by a 7 digit number (e.g. kb1234567)
+	 * 
+	 * TODO talk to parents about how they want assignment groups data to be filtered 
 	 */
 
 	/**
