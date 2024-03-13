9ccad0b2e381b929a8043029f8ccf3ee42222970
zhirst <zach_hirst@live.com>
2024-03-13 16:11:34 -0500

    Added input screening/formatting to the KB field input. Program trims whitespace from the front and back, flashes an error text if the digits after 'kb' are not 7 digits long, and replaces lowecase letters with uppercase.

--- popup.js
+++ popup.js
@@ -23,45 +23,48 @@ document.getElementById('sendButton').addEventListener('click', function() {
 
 	var incValue = globalINC;
 	console.log('INC grabbed: ', incValue);
-	var kbValue = document.getElementById('kb').value;
+	
+	var kbValue = document.getElementById('kb').value.trim();
 	console.log('Raw KB grabbed: ', kbValue, 'Formatting...');
 	var kbnumber = kbValue.slice(2);
+	if(kbnumber.length != 7)
+	{ 
+		document.getElementById('errorMessage').textContent = 'Please enter a valid KB number';
+		console.error('KB number should be exactly 7 digits long'); 
+		return;
+	} else {
+		document.getElementById('errorMessage').textContent = '';
+	}
 	var KB = 'KB' + kbnumber;
 	console.log('Success formatting kbValue: ', KB);
+
 	var assignmentGroupValue = document.getElementById('assignmentgroup').value;
 	console.log('Assignment Group grabbed: ', assignmentGroupValue);
 
 	console.log('Form Values: ', {incValue, KB, assignmentGroupValue});
 
 	var formURL = 'https://forms.office.com/Pages/ResponsePage.aspx?id=mthHA3QB002t6zM5yJw19YVBTy6hCVZEnwuXvhFA35JUODdSNlU0RUlVSlRPUk1MT0w2SktCRVRLQyQlQCN0PWcu';
-	var formData = new formData();
+	var formData = new FormData();
 
 	formData.append('QuestionId_r56340fcad985487aaff9636aad9893e7 QuestionInfo_r56340fcad985487aaff9636aad9893e7', incValue);
 	formData.append('QuestionId_r015fea291c5f49a69cdb24054e9883d2 QuestionInfo_r015fea291c5f49a69cdb24054e9883d2', KB);
 	formData.append('QuestionId_r518dab23ce314eb2818a9c0a9b90c11a QuestionInfo_r518dab23ce314eb2818a9c0a9b90c11a', assignmentGroupValue);
 
 	console.log('Sending form data: ', formData);
-
-	fetch(formUrl, {
+	fetch(formURL, {
         method: 'POST',
         mode: 'no-cors',
         body: formData
     });
 
 	console.log('Form data sent');
-
+	//TODO flash a sent message onto the popup.html 
+	
 	/**
-	 * TODO bug fix: inc value not being thrown in form data object 
 	 * TODO name field addition
 	 * 
-	 * TODO test KB value for correct format
+	 * TODO autofilling form from list/live search of assignment groups 
 	 * 
-	 * autofilling form from list/live search of assignment groups 
 	 * TODO change h3 in popup.html 
-	 */
-
-	/**
-	 * when server sends recieved code, flash a "sent" message
-	 * on popup
 	 */
 })
