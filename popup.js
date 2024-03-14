9033ba7e4e4a3ac9e72d8ebdce5611c18f74b3c9
zhirst <zach_hirst@live.com>
2024-03-14 15:52:05 -0500

    Updated todos

--- popup.js
+++ popup.js
@@ -79,6 +79,7 @@ document.getElementById('sendButton').addEventListener('click', function() {
 })
 
 //TODO flash a sent message onto the popup.html 
-//TODO POST error flashing on main html
+//TODO POST error flashing on main html?
 //TODO name field addition (persistent storage and editing of username in settings page)
 //TODO settings/info page (persistent storage and editing of settings)
+//TODO make a settings icon 
